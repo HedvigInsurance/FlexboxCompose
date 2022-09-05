c937beca74b15db258bdb8ffadd109ca233131ba
Sam Pettersson <sam@sampettersson.com>
2022-09-05 10:06:40 +0200

    Add all scripts

--- scripts/start_emulator.sh
+++ scripts/start_emulator.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-if ! "$ANDROID_HOME/"tools/android list avd | grep -q Pixel_5_API_32; then
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if ! $ANDROID_HOME/tools/android list avd | grep -q Nexus_5X_API_26; then
     echo "No emulator for screenshot tests found, creating one..."
-    ./scripts/create_emulator.sh
+    $DIR/create_emulator.sh
 fi
 
-if "$ANDROID_HOME"/platform-tools/adb devices -l | grep -q emulator; then
+if $ANDROID_HOME/platform-tools/adb devices -l | grep -q emulator; then
     echo "Emulator already running"
     exit 0
 fi
 
 echo "Starting emulator..."
-echo "no" | "$ANDROID_HOME"/emulator/emulator "-avd" "Pixel_5_API_32" "-no-audio" "-no-boot-anim" &
+echo "no" | $ANDROID_HOME/emulator/emulator "-avd" "Nexus_5X_API_26" "-no-audio" "-no-boot-anim" "-gpu" "swiftshader_indirect" &
 
-./scripts/wait_for_emulator.sh
+$DIR/wait_for_emulator.sh
 
 echo "Emulator ready, disabling animations!"
 
-./scripts/disable_animations.sh
+$DIR/disable_animations.sh
 
 echo "Emulator started and ready to rock!"
